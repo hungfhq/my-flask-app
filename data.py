17916fcc4759978e35e2a2753628c003281ec5b7
hung hq <hungfhq@gmail.com>
2019-04-21 06:53:15 +0700

    version run on localhost

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title': 'Article One',
+      'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium minima enim aliquid suscipit dolore voluptas, quis eius velit cupiditate temporibus totam quas rem ipsa cumque quam architecto ea facere facilis.',
+      'author': 'Brad',
+      'create_date': '04-12-2000'
+    },
+    {
+      'id': 2,
+      'title': 'Article Two',
+      'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium minima enim aliquid suscipit dolore voluptas, quis eius velit cupiditate temporibus totam quas rem ipsa cumque quam architecto ea facere facilis.',
+      'author': 'John Doe',
+      'create_date': '04-12-2000'
+    },
+    {
+      'id': 3,
+      'title': 'Article Three',
+      'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium minima enim aliquid suscipit dolore voluptas, quis eius velit cupiditate temporibus totam quas rem ipsa cumque quam architecto ea facere facilis.',
+      'author': 'Brad Media',
+      'create_date': '04-12-2000'
+    }
+  ]
+  return articles
